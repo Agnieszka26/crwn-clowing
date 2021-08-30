f8ba7a290797e64c2dd85ce71f814b7b4bcd26b0
Agnieszka26 <medrek.agn@yahoo.com>
2021-08-30 15:32:00 +0100

    routing and structuring, updating our app with shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component.jsx"
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from './pages/shop/shop.components';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+      
 
     </div>
   );
